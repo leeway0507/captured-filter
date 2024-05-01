06d32b9d2c70994f6f43ee2ca4fcbff9ce138e10
yangwoo lee <679oose@gmail.com>
2024-05-01 14:40:22 +0900

    24/05/01 불필요 파일 제거

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className={`${fontSans.className}`}>
         <main className="min-h-screen flex flex-col relative w-full">
           <MobileWarning />
-          <CustomServiceBlock />
+          {/* <CustomServiceBlock /> */}
           <div className="bg-accent/30 flex flex-col grow justify-between px-1 relative w-full mx-auto " id="main-body">
             {children}
           </div>
